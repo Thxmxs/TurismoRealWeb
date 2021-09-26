3661bc345d4b35508f3224b3361171b469b71f32
Thxmxs <thomas.c.arcos@gmail.com>
2021-09-26 17:58:04 -0300

    Add project files.

---
+++ TurismoRealWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TurismoRealWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TurismoRealWeb/Models/UsuarioModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TurismoRealWeb.Models
+{
+    public class UsuarioModel
+    {
+        public String Email { get; set; }
+        public int Id { get; set; }
+
+        public int Password { get; set; }
+    }
+}
